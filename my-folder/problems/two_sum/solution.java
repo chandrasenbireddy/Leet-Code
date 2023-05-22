9b404654244cefa9872b36b22f43fbb192396648
Chandrasen <37106400+chandrasenbireddy@users.noreply.github.com>
2023-05-22 23:11:04 +0000

    Sync LeetCode submission - Two Sum (java)

--- my-folder/problems/two_sum/solution.java
+++ my-folder/problems/two_sum/solution.java
@@ -1,13 +1,46 @@
+
 class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        for(int i =0; i<= nums.length; i++){
-            for (int j = i+1; j<nums.length; j++){
-                if (nums[j] == target - nums[i]) {
-                    return new int[] {i,j};
+    static int binarysrch(int[] nums,int start, int target){
+        start = start+1;
+        int end = nums.length - 1;
+        if (nums[end] == target){
+            return end;
+        }
+
+        while(start < end){
+            int mid = (start + end) / 2;
+            if (nums[start] == target){
+                return start;
+            }
+            else if (nums[end] == target) {
+                System.out.println(start);
+                return end;
+            }
+            else {
+                if (nums[mid] == target){
+                    return mid;
+                }
+                else if (target > nums[mid]){
+                    start = mid+1;
+                }
+                else{
+                    end = mid;
                 }
             }
         }
-        return null;
-
+        return -1;
     }
+
+    public int[] twoSum(int[] nums, int target) {
+        int index;
+        for(int i =0; i< nums.length; i++){
+            int value = target - nums[i];
+            int start = i;
+            index = binarysrch(nums, start, value);
+        if (index != -1){
+            return new int[] {i,index}; 
+        }
+    }
+        return null;
+}
 }
